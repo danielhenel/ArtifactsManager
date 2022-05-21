73a9f2371b17f76ad9e810fefb88489945ffaaad
Daniel Henel <daniel-henel@protonmail.com>
2022-05-21 17:47:36 +0200

    created User class

---
+++ ArtifactsManager/ArtifactsManager/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtifactsManager
+{
+    public class User
+    {
+        private string login { get; set; }  
+        private string password { get; set; }  
+        private string role { get; set; } 
+        public bool canRemoveArtifacts { get; set; }
+        public bool canEditArtifacts { get; set; }
+        public bool canAddArtifacts { get; set; }
+    }
+}

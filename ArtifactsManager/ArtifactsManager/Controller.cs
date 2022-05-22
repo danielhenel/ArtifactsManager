6ae43cc78f844b2b4aaa5c3e8989c709992cb5b8
Daniel Henel <daniel-henel@protonmail.com>
2022-05-22 09:55:46 +0200

    initial controller class

---
+++ ArtifactsManager/ArtifactsManager/Controller.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtifactsManager
+{
+    internal class Controller
+    {
+        private User loggedUser { get; set; }
+        private string currentTable { get; set; } 
+    }
+}
